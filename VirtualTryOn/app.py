628a200c050a233d3c0c1e3819219e63b77d7b23
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2024-05-03 23:13:37 +0530

    removed token

--- VirtualTryOn/app.py
+++ VirtualTryOn/app.py
@@ -23,7 +23,7 @@ import retry
 
 app = Flask(__name__)
 
-os.environ["REPLICATE_API_TOKEN"] = "r8_ImbW66hCVSAH1rFeoZPD8kmCFXUVil40KIXTB"
+os.environ["REPLICATE_API_TOKEN"] = "token"
 machine_number = 0
 load_dotenv()
 localStorage = localStoragePy('OutfitAnyone', 'text')
